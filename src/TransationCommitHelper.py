ff26b70fc09682b1f40b6293fa30b1b717d35e7c
Phospher Lau <phospher@qq.com>
2013-11-08 06:22:19 -0500

    Pharse one reply checks

--- src/TransationCommitHelper.py
+++ src/TransationCommitHelper.py
@@ -4,7 +4,9 @@ import thread;
 PROTOCOL_MESSAGE_PREPARE =  'PREPARE'
 PROTOCOL_MESSAGE_ABORT = 'ABORT'
 PROTOCOL_MESSAGE_COMMIT = 'COMMIT'
+PROTOCOL_MESSAGE_ROLLBACK = 'ROLLBACK'
 PROTOCOL_MESSAGE_ACKCOMMIT = 'ACK-COMMIT'
+PROTOCOL_MESSAGE_PREPARED = 'PREPARED'
 TIME_OUT = 5000
 
 class TransatonCommitCoordinator(object):
@@ -13,7 +15,7 @@ class TransatonCommitCoordinator(object):
 		self.communicateFunc = communicateFunc
 		self.condition = threading.Condition()
 	
-	def __checkAllReply(clientReply):
+	def __checkAllReply(self, clientReply):
 	    i = 0
 	    for key in clientReply:
 	        i += 1 if not clientReply[key] else 0
@@ -23,23 +25,39 @@ class TransatonCommitCoordinator(object):
 	    self.__pharseOneCommit(clientList)
 	
 	def __pharseOneCommit(self, clientList):
-	    clientReplay = {};
+	    pharseOneReplay = {};
 	    for client in clientList:
-	        clientReplay[client[0]] = None
+	        pharseOneReplay[client] = None
 	    self.condiction.acquire()
 		for client in clientList:
 		    thread.start_new_thread(self.__sendPrepare, (client, clientReplay))
 		self.condiction.wait()
 		self.condiction.release()
-		
-	def __sendPrepare(self, client, clientReply):
+	
+	def __checkAllRepared(self, clientReplay):
+	    i = 0
+	    for key in clientReplay:
+	        i += 1 if clientReply[key] == PROTOCOL_MESSAGE_PREPARED else 0
+	    return True if i == len(clientReply.keys()) else False
+	 
+	def __pharseTwoCommit(self, clientList, pharseOneReplay):
+	    pass
+	
+	def __sendMessageClient(self, client, message, exceptMsg, clientReply):
 	    try:
-	       reply = self.communicateFunc(client, PROTOCOL_MESSAGE_PREPARE, TIME_OUT)
+	       reply = self.communicateFunc(client, message, TIME_OUT)
 	    except:
-	        reply = PROTOCOL_MESSAGE_ABORT
-	    clientReply[ip] = reply
-	    if __checkAllReply(clientReply):
+	        reply = exceptMsg
+	    clientReply[client] = reply
+	    if self.__checkAllReply(clientReply):
 	        condition.acquire()
 	        condition.notify()
 	        condition.release()
+		
+	def __sendPrepare(self, client, clientReply):
+	    self.__sendMessageClient(client, PROTOCOL_MESSAGE_PREPARE, PROTOCOL_MESSAGE_ABORT, clientReply)
+	 
+	def __sendPhaseTwoMessage(self, client, message, clientReplay):
+	    self.__sendMessageClient(client, message, None, clientReplay)
+	        
 	        
